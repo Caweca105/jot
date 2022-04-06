76f28190abec6c473d34a00070b81477a33a7d0a
Caweca105 <lemos.diogo105@gmail.com>
2022-04-06 15:35:50 +0100

    tried to solve some errors, without success

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
+require('./bootstrap');
 import Vue from 'vue';
 import router from './router';
 import App from './components/App';
+window.Vue = require('vue');
 
-require('./bootstrap');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 const app = new Vue({
     el: '#app',
